934e2ece151e0610a0cd350b7ce36cac32a86f5e
AbhinavSenGitHub <abhinavsen987@gmail.com>
2024-01-22 23:18:07 +0530

    test commit (5)

--- services/common.js
+++ services/common.js
@@ -12,7 +12,7 @@ exports.cookieExtractor = function (req) {
     let token = null;
     if (req && req.cookies)
     {
-    token = req. cookies ['jwt'];
+    token = req.cookies ['jwt'];
     }
     // token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1YWQ3MzAyMTM5ODQxOTM4MWUwNTFkOCIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzA1OTM1MDUwfQ.4zovWoWSn5HATGkebI67uAfYhy_gvwpMSiUhZI0R5X4; Path=/; Expires=Mon, 22 Jan 2024 15:50:50 GMT; HttpOnly"
     return token;
